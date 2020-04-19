efbe8bfbb882360cc437e2ad1b5ed1e00d277de5
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-04-19 10:51:54 +0200

    reset

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>movies</div>;
+
+export default App;
--- src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as About } from './About';
-export { default as Error404 } from './Error404';
-export { default as Movies } from './Movies';
