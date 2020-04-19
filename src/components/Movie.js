e20ac18d66631511a83c57a99e3ee6dadbaccba1
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-04-19 17:59:24 +0200

    refactor - movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-// Components
 import Navigation from './elements/Navigation';
 import MovieInfo from './elements/MovieInfo';
 import MovieInfoBar from './elements/MovieInfoBar';
-import Actor from './elements/Actor';
 import Grid from './elements/Grid';
+import Actor from './elements/Actor';
 import Spinner from './elements/Spinner';
 
 import { useMovieFetch } from './hooks/useMovieFetch';
 
 const Movie = ({ movieId }) => {
-  const [movie, loading, error] = useMovieFetch(movieId);
+  const [movie, isLoading, error] = useMovieFetch(movieId);
 
+  // TODO: return NotFound page
   if (error) return <div>Something went wrong ...</div>;
-  if (loading) return <Spinner />;
+  if (isLoading) return <Spinner />;
 
   return (
   <>
@@ -28,7 +28,7 @@ const Movie = ({ movieId }) => {
     <Grid header="Actors">
       {movie.actors.map(actor => (
         <Actor key={actor.credit_id} actor={actor} />
-      ))}     
+      ))}
     </Grid>
   </>
   )
--- src/components/hooks/useMovieFetch.js
+++ src/components/hooks/useMovieFetch.js
@@ -1,40 +1,55 @@
 import { useState, useEffect, useCallback } from 'react';
 import { BASE_URL_API, API_KEY } from '../../config';
 
-export const useMovieFetch = movieId => {
+export const useMovieFetch = (movieId) => {
   const [state, setState] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
 
   const fetchData = useCallback(async () => {
     setError(false);
-    setLoading(true);
+    setIsLoading(true);
 
     try {
-      const endpoint = `${BASE_URL_API}movie/${movieId}?api_key=${API_KEY}`;
-      const result = await (await fetch(endpoint)).json();
+      const endpointMovieData = `${BASE_URL_API}movie/${movieId}?api_key=${API_KEY}`;
+      const resultMovieData = await (await fetch(endpointMovieData)).json();
+      console.log(resultMovieData);
 
-      const creditsEndpoint = `${BASE_URL_API}movie/${movieId}/credits?api_key=${API_KEY}`;
-      const creditsResult = await (await fetch(creditsEndpoint)).json();
-      const directors = creditsResult.crew.filter(
-        member => member.job === 'Director'
+      const endpointCredits = `${BASE_URL_API}movie/${movieId}/credits?api_key=${API_KEY}`;
+      const resultCredits = await (await fetch(endpointCredits)).json();
+      console.log(resultCredits);
+
+      const directors = resultCredits.crew.filter(
+        (member) => member.job === 'Director',
       );
 
       setState({
-        ...result,
-        actors: creditsResult.cast,
+        ...resultMovieData,
+        actors: resultCredits.cast,
         directors,
-      })
-
+      });
     } catch (error) {
       setError(true);
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
-    setLoading(false);
-  }, [movieId])
+  }, [movieId]);
 
   useEffect(() => {
-    fetchData();
-  }, [fetchData])
+    if (localStorage[movieId]) {
+      setState(JSON.parse(localStorage[movieId]));
+      setIsLoading(false);
+      console.log('data from localStorage');
+    } else {
+      fetchData();
+      console.log('data from API');
+    }
+  }, [fetchData, movieId]);
 
-  return [state, loading, error];
-}
+  useEffect(() => {
+    localStorage.setItem(movieId, JSON.stringify(state));
+  }, [movieId, state]);
+
+  return [state, isLoading, error];
+};
