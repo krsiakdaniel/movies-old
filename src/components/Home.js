d0ebcc8b4d4b08e4c10e0a8461aee61b3ccbe7ff
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-04-19 12:00:14 +0200

    home - hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import {
-    API_URL,
-    API_KEY,
-    API_BASE_URL,
-    POSTER_SIZE,
-    BACKDROP_SIZE
+  API_URL,
+  API_KEY,
+  API_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE,
+  IMAGE_BASE_URL,
 } from '../config';
 
 // import Components
@@ -19,20 +20,27 @@ import Spinner from './elements/Spinner';
 import { useHomeFetch } from './hooks/useHomeFetch';
 
 const Home = () => {
-  const [{ state, loading, error}, fetchMovies] = useHomeFetch();
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
 
   console.log(state);
 
+  if (error) return <div>Something went wrong ...</div>;
+  if (!state.movies[0]) return <Spinner />;
+
   return (
     <>
-      <HeroImage />
+      <HeroImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
       <SearchBar />
       <Grid />
       <MovieThumb />
       <Spinner />
       <LoadMoreBtn />
     </>
-  )
-}
+  );
+};
 
 export default Home;
