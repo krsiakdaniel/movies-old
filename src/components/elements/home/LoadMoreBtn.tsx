717c61a4b38c623bbce0279ddbe1d22f8fddd246
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-05-02 18:59:58 +0200

    LoadMoreBtn

--- src/components/elements/home/LoadMoreBtn.tsx
+++ src/components/elements/home/LoadMoreBtn.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '@chakra-ui/core';
 import { css } from '@emotion/core';
 
 type Props = {
@@ -8,29 +9,22 @@ type Props = {
 
 const LoadMoreBtn = ({ text, callback }: Props) => {
   return (
-    <button css={cssLoadMoreBtn} type="button" onClick={callback}>
+    <Button
+      type="button"
+      variantColor="green"
+      css={cssLoadMoreBtn}
+      onClick={callback}
+    >
       {text}
-    </button>
+    </Button>
   );
 };
 
 const cssLoadMoreBtn = css({
-  background: '#27ae60',
-  border: 'transparent',
   minWidth: 150,
-  color: '#fff',
   cursor: 'pointer',
-  transition: 'all 0.4s',
-  borderRadius: 4,
-  fontSize: 24,
   display: 'block',
   margin: '32px auto',
-  padding: '4px 8px',
-  outline: 'none',
-
-  ':hover': {
-    background: '#2ecc71',
-  },
 
   '@media screen and (max-width: 768px)': {
     margin: '24px auto',
