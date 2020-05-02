41b8598161950a780899ed7baf79f42c7c810a64
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-05-02 20:24:57 +0200

    footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Box, Text } from '@chakra-ui/core';
 import { css } from '@emotion/core';
 import { useTranslation } from 'react-i18next';
-
 import { Link } from 'react-router-dom';
 
 import Emoji from 'a11y-react-emoji';
@@ -11,30 +11,25 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <div css={cssFooter}>
-      <p>
+    <Box css={cssFooter}>
+      <Text fontSize="sm">
         {t('footerMadeIn')} <Emoji symbol="🇨🇿" label="Czech Republic" />
-        {t('footerMadeBy')} <Link to={pageUrls.about}>{t('appAuthor')}</Link>
-      </p>
-      <p css={cssTMDb}>{t('footerTMDb')}</p>
-    </div>
+        {t('footerMadeBy')}
+        <Link to={pageUrls.about}>{t('appAuthor')}</Link>
+      </Text>
+      <Text fontSize="xs" css={cssTMDb}>
+        {t('footerTMDb')}
+      </Text>
+    </Box>
   );
 };
 
+// TODO: use chakra
 const cssFooter = css({
-  maxWidth: 1200,
-  margin: '0 auto',
-  padding: '0 16px',
-  fontSize: 12,
   textAlign: 'center',
-
-  '@media screen and (max-width: 768px)': {
-    padding: '0 24px',
-  },
 });
 
 const cssTMDb = css({
-  fontSize: 10,
   color: '#bdc3c7',
 });
 
