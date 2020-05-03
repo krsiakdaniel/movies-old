3e89f154ea7a5dae144a5528884367d292914cb8
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-05-03 17:28:57 +0200

    codacy - search - fix 'was used before it was defined'
    Disallow Early Use (no-use-before-define) - In JavaScript, prior to ES6, variable and function declarations are hoisted to the top of a scope, so it's possible to use identifiers before their formal declarations in code. This can be confusing and some believe it is best to always declare variables and functions before using them.

--- src/components/elements/home/SearchBar.js
+++ src/components/elements/home/SearchBar.js
@@ -6,6 +6,32 @@ import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import { cypress } from '../../../config';
 
+// TODO: use chakra
+const cssSearchBarRow = css({
+  width: '100%',
+});
+
+const cssSearchBar = css({
+  maxWidth: 1200,
+  margin: '0 auto',
+  padding: '0 16px',
+
+  '@media screen and (max-width: 768px)': {
+    padding: '0 24px',
+  },
+});
+
+const cssSearchBarInput = css({
+  margin: '32px 0',
+  transition: 'all 0.4s',
+
+  ':focus': {
+    outline: 0,
+    borderColor: '#27ae60',
+    boxShadow: '0 0 0 2px rgba(39, 174, 96, 0.1)',
+  },
+});
+
 const SearchBar = ({ callback }) => {
   const { t } = useTranslation();
 
@@ -41,31 +67,5 @@ const SearchBar = ({ callback }) => {
 SearchBar.propTypes = {
   callback: PropTypes.func,
 };
-
-// TODO: use chakra
-const cssSearchBarRow = css({
-  width: '100%',
-});
-
-const cssSearchBar = css({
-  maxWidth: 1200,
-  margin: '0 auto',
-  padding: '0 16px',
-
-  '@media screen and (max-width: 768px)': {
-    padding: '0 24px',
-  },
-});
-
-const cssSearchBarInput = css({
-  margin: '32px 0',
-  transition: 'all 0.4s',
-
-  ':focus': {
-    outline: 0,
-    borderColor: '#27ae60',
-    boxShadow: '0 0 0 2px rgba(39, 174, 96, 0.1)',
-  },
-});
 
 export { SearchBar };
