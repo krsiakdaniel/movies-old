487a22ef77593349d3f481d8c9f98ca130367688
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-05-03 14:02:14 +0200

    MovieInfo

--- src/components/elements/movie/MovieInfo.js
+++ src/components/elements/movie/MovieInfo.js
@@ -1,63 +1,62 @@
 import React from 'react';
+import { Box, Heading, List, ListItem, Text } from '@chakra-ui/core';
 import { css } from '@emotion/core';
-import { useTranslation } from 'react-i18next';
-
 import { MovieThumb } from '../shared';
+import { useTranslation } from 'react-i18next';
 
 import PropTypes from 'prop-types';
 import { calcTime, convertMoney } from '../../../utils';
-import { BASE_URL_IMAGE, SIZE_BACKDROP, SIZE_POSTER } from '../../../config';
+import { BASE_URL_IMAGE, SIZE_POSTER } from '../../../config';
 
-import NoImage from '../../../assets/svg/error/no-image.svg';
+import imgNoPoster from '../../../assets/svg/error/no-image.svg';
 
 const MovieInfo = ({ movie, time, budget, revenue }) => {
   const { t } = useTranslation();
 
   return (
-    <div css={cssMovieBackdrop} backdrop={movie.backdrop_path}>
-      <div css={cssMovieInfo}>
-        <div css={cssMovieInfoThumb}>
+    <Box css={cssMovieBackdrop} backdrop={movie.backdrop_path}>
+      <Box css={cssMovie}>
+        <Box css={cssMovieThumbWrap}>
           <MovieThumb
             image={
               movie.poster_path
                 ? `${BASE_URL_IMAGE}${SIZE_POSTER}${movie.poster_path}`
-                : NoImage
+                : imgNoPoster
             }
             isClickable={false}
             alt={movie.title}
+            css={cssMovieThumb}
           />
-        </div>
+        </Box>
 
-        <div css={cssMovieInfoText}>
-          <h1>{movie.title}</h1>
-          <p>{movie.overview}</p>
+        <Box css={cssMovieInfo}>
+          <Heading css={cssMovieTitle}>{movie.title}</Heading>
+          <Text css={cssMovieOverview}>{movie.overview}</Text>
 
-          <ul css={cssMovieInfoList}>
-            <li>
-              <strong>{t('movie.rating')}: </strong> {movie.vote_average}
-            </li>
-            <li>
-              <strong>{t('movie.director')}: </strong>
+          <List css={cssMovieDetails}>
+            lineHeight: 1,
+            <ListItem>
+              <strong>{t('movie.director')}:</strong>
               {movie?.directors?.map((director) => (
-                <span key={director.credit_id}>
+                <Text key={director.credit_id}>
                   {director.name}
                   {movie.directors.length > 1 ? ', ' : ''}
-                </span>
+                </Text>
               ))}
-            </li>
-            <li>
-              <strong>{t('movie.runningTime')}: </strong> {calcTime(time)}
-            </li>
-            <li>
-              <strong>{t('movie.budget')}: </strong> {convertMoney(budget)}
-            </li>
-            <li>
-              <strong>{t('movie.revenue')}: </strong> {convertMoney(revenue)}
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+            </ListItem>
+            <ListItem>
+              <strong>{t('movie.runningTime')}:</strong> {calcTime(time)}
+            </ListItem>
+            <ListItem>
+              <strong>{t('movie.budget')}:</strong> {convertMoney(budget)}
+            </ListItem>
+            <ListItem>
+              <strong>{t('movie.revenue')}:</strong> {convertMoney(revenue)}
+            </ListItem>
+          </List>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
@@ -66,23 +65,20 @@ MovieInfo.propTypes = {
   directors: PropTypes.array,
 };
 
+// TODO: use chakra
 const cssMovieBackdrop = css({
   // TODO: fix bg
-  background: `${(props) =>
-    props.backdrop
-      ? `url('${BASE_URL_IMAGE}${SIZE_BACKDROP}${props.backdrop}')`
-      : '#34495e'}`,
-  backgroundSize: 'cover !important',
-  backgroundPosition: 'center !important',
+  // background: `${(props) =>
+  //   props.backdrop
+  //     ? `url('${BASE_URL_IMAGE}${SIZE_BACKDROP}${props.backdrop}')`
+  //     : '#34495e'}`,
+  // backgroundSize: 'cover !important',
+  // backgroundPosition: 'center !important',
+  background: '#edf2f7', // gray100
   width: '100%',
   padding: '32px 16px',
   marginBottom: 32,
-  animation: 'animateMovieInfo 1s',
-
-  '@media screen and (max-width: 768px)': {
-    padding: '32px 24px',
-  },
-
+  // animation: 'animateMovieInfo 1s',
   // TODO: fix
   // @keyframes animateMovieInfo {
   //   from {
@@ -92,36 +88,56 @@ const cssMovieBackdrop = css({
   //     opacity: 1,
   //   }
   // }
+
+  '@media screen and (max-width: 768px)': {
+    padding: '32px 24px',
+  },
 });
 
-const cssMovieInfo = css({
+const cssMovie = css({
   maxWidth: 1200,
   minHeight: 450,
   margin: '0 auto',
-  background: 'rgb(0, 0, 0, 0.7)',
+  background: '#2d3748',
   borderRadius: 8,
   position: 'relative',
 });
 
-const cssMovieInfoThumb = css({
+const cssMovieThumbWrap = css({
   width: 300,
   float: 'left',
 
   '@media screen and (max-width: 768px)': {
     width: '100%',
+    marginBottom: 16,
   },
 });
 
-const cssMovieInfoText = css({
-  padding: 40,
+const cssMovieThumb = css({
+  borderRadius: 8,
+});
+
+const cssMovieInfo = css({
   color: '#fff',
-  overflow: 'hidden',
+  marginLeft: 300,
+  padding: 32,
+
+  '@media screen and (max-width: 768px)': {
+    marginLeft: 0,
+  },
 });
 
-const cssMovieInfoList = css({
-  li: {
-    marginBottom: 4,
-  },
+const cssMovieTitle = css({
+  marginBottom: 16,
+});
+
+const cssMovieOverview = css({
+  marginBottom: 16,
+  lineHeight: 1,
+});
+
+const cssMovieDetails = css({
+  lineHeight: 1,
 });
 
 export { MovieInfo };
